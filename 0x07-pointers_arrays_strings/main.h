ddfce8b6e57648b94ff10d8a7c5d76e80cc3aa06
nuradinsultan <nuradinsultan2@gmail.com>
2023-01-02 03:47:11 -0800

    well good work

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,47 +1,65 @@
-fndef INV_MAIN_H
+#ifndef INV_HOLBERTON_H
 
-#define INV_MAIN_H
+#define INV_HOLBERTON_H
 
 
 
 int _putchar(char);
 
+char *_strcat(char *dest, char *src);
 
+char *_strncat(char *dest, char *src, int n);
 
-char *_memset(char *s, char b, unsigned int n);
+char *_strncpy(char *dest, char *src, int n);
 
+int _strcmp(char *s1, char *s2);
 
+void reverse_array(int *a, int n);
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *string_toupper(char *);
 
+char *cap_string(char *);
 
+char *leet(char *);
 
-char *_strchr(char *s, char c);
+char *rot13(char *);
 
+void print_number(int n);
 
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
-unsigned int _strspn(char *s, char *accept);
+void print_buffer(char *b, int size);
 
-char *_strpbrk(char *s, char *accept);
+#endif#ifndef INV_HOLBERTON_H
 
-
-
-char *_strstr(char *haystack, char *needle);
-
-
-
-void print_chessboard(char (*a)[8]);
-
-
-
-void print_diagsums(int *a, int size);
-
-
-
-void set_string(char **s, char *to);
+#define INV_HOLBERTON_H
 
 
 
-int coincidence(char *a, char *b);
+int _putchar(char);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
+char *rot13(char *);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
 
 #endif

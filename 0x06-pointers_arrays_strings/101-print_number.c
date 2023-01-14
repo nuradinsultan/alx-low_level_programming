3ff44a4df9c408a84248f3f1d7c5c2e5a2865f2b
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-01-14 21:24:12 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,97 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- *
- *  * print_number - print integer
- *
- *   * Return: void
- *
- *    * @n: number
- *
- *     */
-
-void print_number(int n)
-
+int main(void)
 {
+  int n;
+  int a[5];
+  int *p;
 
-		unsigned int size = n;
-
-
-
-			int digits = 0;
-
-
-
-				int i;
-
-
-
-					if (n < 0)
-
-								size = -n;
-
-						if (n == 0)
-
-									digits = 1;
-
-
-
-							while (size >= 1)
-
-									{
-
-												size = size / 10;
-
-														digits++;
-
-															}
-
-
-
-								for (i = 0; i < digits; i++)
-
-										{
-
-
-
-													int pow = 1;
-
-
-
-															int j;
-
-
-
-																	int d;
-
-
-
-																			for (j = 0; j < digits - i - 1; j++)
-
-																						{
-
-																										pow = pow * 10;
-
-																												}
-
-
-
-																					d = ((n / pow) % 10);
-
-																							if (n < 0)
-
-																										{
-
-																														d = -d;
-
-																																	if (i == 0)
-
-																																						_putchar(45);
-
-																																			}
-
-																									_putchar(48 + d);
-
-																										}
-
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }

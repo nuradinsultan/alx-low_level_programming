04599fbddca1db67d78bdb56c72ab408e1f1a2a2
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-01-14 16:11:11 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,10 +4,11 @@
  * @n: digit to find the last place of.
  * Return: The last digit.
  */
-int print_last_digit(int n)
+int print_last_digit(int)
 {
+        int n;
 	if (n < 0)
 		n = n * -1;
 	_putchar((n % 10) + '0');
-	return (n);
+	return (n % 10);
 }

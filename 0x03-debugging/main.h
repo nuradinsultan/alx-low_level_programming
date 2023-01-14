2f41c384d5a30deb7c27ca143b3c34dd4ef8be63
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-01-14 17:54:33 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-
-void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year); int day, int year);
 
 #endif /* MAIN_H */

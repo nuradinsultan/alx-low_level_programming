9f214afa3f91b465c3da8713ede35d5e013633cc
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-01-14 14:49:44 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,38 +1,17 @@
 #include "main.h"
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
 int main(void)
-
 {
+	char c[] = "Holberton";
+	int i;
 
-      _putchar('_');
-
-       _putchar('p');
-
-       _putchar('u');
-
-	_putchar('t');
-
-       _putchar('c');
-
-       _putchar('h');
-
-	_putchar('a');
-
-       _putchar('r');
-
-       _putchar('\n');
-											
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
-
 }
-

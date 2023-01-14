5c52e69386927c81e41bc7501723516d96c24954
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-01-14 21:07:19 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,57 +1,39 @@
 #include "main.h"
 
 /**
+ * print_number - prints a number with _putchar
+ * @n: Number to print
  *
- *  * rot13 - rot13 encoding
- *
- *   * Return: pointer to arr
- *
- *    * @s: string
- *
- *     */
-
-char *rot13(char *s)
-
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_number(int n)
 {
+	int ten, i, neg;
+	unsigned int digit, digit2;
 
-		char half1[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
-
-
-
-			char half2[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
-
-
-
-				int i = 0, j;
-
-
-
-					while (s[i] != 0)
-
-							{
-
-										char c = s[i];
-
-
-
-												for (j = 0; j < 52; j++)
-
-															{
-
-																			if (c == half1[j])
-
-																							{
-
-																												s[i] = half2[j];
-
-																															}
-
-																					}
-
-														i++;
-
-															}
-
-						return (s);
-
+	ten = 1;
+	if (n < 0)
+	{
+		n = n * -1;
+		neg = 1;
+	}
+	digit = n;
+	digit2 = n;
+	while (digit >= 10)
+	{
+		digit = digit / 10;
+		ten = ten * 10;
+	}
+	if (neg == 1)
+	{
+		_putchar('-');
+	}
+	_putchar('0' + (digit2 / ten));
+	i = ten / 10;
+	while (i >= 1)
+	{
+		_putchar('0' + (digit2 / i) % 10);
+		i = i / 10;
+	}
 }

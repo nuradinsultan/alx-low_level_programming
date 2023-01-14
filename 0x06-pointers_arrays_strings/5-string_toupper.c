1968a9a051bb588a52be862d9b55cc48c255df4e
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-01-14 20:58:36 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,39 +1,23 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
+ * string_toupper - changes lowercase letters to uppercase in string
+ * @s: string to change
  *
- *  * string_toupper - convert to uppercase
- *
- *   * Return: void
- *
- *    * @s: string
- *
- *     */
-
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 char *string_toupper(char *s)
-
 {
+	int i, j;
 
-		int i = 0;
-
-
-
-			while (s[i] != 0)
-
-					{
-
-								if (s[i] >= 'a' && s[i] <= 'z')
-
-											{
-
-															s[i] = 'A' + (s[i] - 'a');
-
-																	}
-
-										i++;
-
-											}
-
-				return (s);
-
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	for (j = 0; j < i; j++)
+	{
+		if ((s[j] >= 97) && (s[j] <= 122))
+			s[j] = s[j] - 32;
+	}
+	return (s);
 }

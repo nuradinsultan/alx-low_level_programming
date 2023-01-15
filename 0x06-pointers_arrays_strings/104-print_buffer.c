c26b8cf4c9ae12db0bf403cc6f75a4c5c04a76f4
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-01-15 13:23:07 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,93 +1,49 @@
+#include "main.h"
 #include <stdio.h>
-
-
-
 /**
+ * infinite_add - adds two numbers
+ * @n1: number 1
+ * @n2: number 2
+ * @r: buffer to store result
+ * @size_r: size of buffer
  *
- *  * print_buffer - Print the entire buffer w/ certain conditions
- *
- *   * @b: The buffer to print
- *
- *    * @size: The size of the buffer
- *
- *     */
-
-void print_buffer(char *b, int size)
-
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int i, j, diff, big, small, cyc, cyc2, count, tmp, ten, check;
+	char *less, *more;
 
-		int i, j;
-
-
-
-			i = 0;
-
-				if (size <= 0)
-
-							putchar('\n');
-
-					else
-
-							{
-
-										while (i < size)
-
-													{
-
-																	printf("%08x: ", i);
-
-																				j = 0;
-
-																							while (j < 10)
-
-																											{
-
-																																if (j % 2 == 0 && j > 0)
-
-																																						printf(" ");
-
-																																				if (j + i > size - 1)
-
-																																										printf("  ");
-
-																																								else
-
-																																														printf("%.2x", b[j + i]);
-
-																																												j++;
-
-																																															}
-
-																										putchar(' ');
-
-																													j = 0;
-
-																																while (j < 10)
-
-																																				{
-
-																																									if (j + i > size - 1)
-
-																																															break;
-
-																																													if (b[j + i] >= ' ' && b[j + i] <= '~')
-
-																																																			putchar(b[j + i]);
-
-																																																	else
-
-																																																							putchar('.');
-
-																																																					j++;
-
-																																																								}
-
-																																			putchar('\n');
-
-																																						i += 10;
-
-																																								}
-
-											}
-
+	i = 0, j = 0;
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i >= j)
+		big = i, small = j, diff = i - j + 1, less = n2, more = n1;
+	else
+		big = j, small = i, diff = j - i + 1, less = n1, more = n2;
+	count = 0, cyc = diff, ten = 0;
+	for (count = 0; count < small; count++, cyc++)
+		r[cyc] = less[count];
+	for (cyc2 = big; cyc2 >= 1; cyc2--)
+	{
+		if (cyc > size_r - 1)
+			return (0);
+		if (r[cyc2] <= 57 && r[cyc2] >= 48)
+			tmp = (r[cyc2] - '0') + (more[cyc2 - 1] - '0') + ten;
+		else
+			tmp = (more[cyc2 - 1] - '0') + ten;
+		ten = tmp / 10;
+		tmp = tmp - (ten * 10);
+		r[cyc2] = '0' + tmp;
+	}
+	r[cyc2] = '0' + ten;
+	if (ten == 0)
+		r++;
+	for (check = 0; check < size_r; check++)
+		if (r[check] > 57 || r[check] < 48)
+			r[check] = 0;
+	return (r);
 }

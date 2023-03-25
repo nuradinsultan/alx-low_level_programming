70d3bf6bd1d9522d0fde22f590f901797df587da
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-03-25 23:22:34 +0300

    Delete 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -P .. https://raw.githubusercontent.com/monoprosito/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../101-make_me_win.sh
-
